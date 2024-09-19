1491f8cfaa49cb455e8ebfccb45536320ac4d8bc
yingxingxin <ssem1722@gmail.com>
2024-09-19 17:50:16 -0400

    commit

--- src/main/java/org/example/csc311_hw3_groupproject/HelloApplication.java
+++ src/main/java/org/example/csc311_hw3_groupproject/HelloApplication.java
@@ -3,6 +3,7 @@ package org.example.csc311_hw3_groupproject;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -11,7 +12,8 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-
+        Image maze = new Image("maze.png");
+        Image robot = new Image("robot.png");
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Robot");
         stage.setScene(scene);
