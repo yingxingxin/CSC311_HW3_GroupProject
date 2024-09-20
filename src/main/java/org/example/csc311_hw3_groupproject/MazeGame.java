44aa3ea543319308ae78caf3db7dc4ac60728927
maradC <maradiagachristopher5@gmail.com>
2024-09-19 21:06:49 -0400

    Set size fr robot and map

--- src/main/java/org/example/csc311_hw3_groupproject/MazeGame.java
+++ src/main/java/org/example/csc311_hw3_groupproject/MazeGame.java
@@ -13,14 +13,18 @@ public class MazeGame extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(MazeGame.class.getResource("hello-view.fxml"));
-         Image mazeImage = new Image("maze.png");
-        Image robot = new Image("robot.png");
+         Maze maze = new Maze("maze.png");
+         ImageView mazeView = maze.getView();
 
-        ImageView mazeView = new ImageView(mazeImage);
-        mazeView.setPreserveRatio(true);
-        mazeView.setFitWidth(600);
+        Image robot = new Image("robot.png");
+        ImageView robotView = new ImageView(robot);
+        robotView.setFitWidth(20);  // Set robot size
+        robotView.setFitHeight(20);
+        robotView.setX(50);  // Initial position for the robot
+        robotView.setY(50);
+
         //Comment
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load(), 600, 550);
         stage.setTitle("Robot");
         stage.setScene(scene);
         stage.show();
