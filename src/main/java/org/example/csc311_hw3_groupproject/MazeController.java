7bc75bf852b55b45433152fbc8ae9386fdc3a8be
maradC <maradiagachristopher5@gmail.com>
2024-09-19 21:05:58 -0400

    loaded image of map and robot. implemented the start position of the robot

--- src/main/java/org/example/csc311_hw3_groupproject/MazeController.java
+++ src/main/java/org/example/csc311_hw3_groupproject/MazeController.java
@@ -2,13 +2,24 @@ package org.example.csc311_hw3_groupproject;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
 public class MazeController {
     @FXML
-    private Label welcomeText;
+    private ImageView mazeView;  // Refers to the maze image in FXML
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private ImageView robotView;  // Refers to the robot image in FXML
+
+    public void initialize() {
+        // Initialize your maze and robot positions if necessary
+        mazeView.setImage(new Image("maze.png"));
+        robotView.setImage(new Image("robot.png"));
+        robotView.setX(50);  // Initial robot position
+        robotView.setY(50);
     }
+
+
+
 }
